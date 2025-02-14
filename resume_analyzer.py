273b1ce0ed5527aafa2c844a002f0c47ab0c25fe
Mayur singh Jadon <mayursinghjadon0907@gmail.com>
2025-02-14 16:33:49 +0530

    ggt

--- resume_analyzer.py
+++
@@ -1,8 +0,0 @@
-def analyze_resumes(job_description, resumes):
-    # Placeholder for the actual analysis logic
-    # Here we simply mock the ranking based on some dummy logic
-    def relevance_score(resume, job_description):
-        return sum(1 for word in job_description.split() if word in resume)
-
-    ranked_resumes = sorted(resumes, key=lambda resume: relevance_score(resume, job_description), reverse=True)
-    return ranked_resumes
